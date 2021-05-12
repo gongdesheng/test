7a3be1100483d5a56ca5cea5e271b83e32cdcb00
Poker-Faces <33587459+Poker-Faces@users.noreply.github.com>
2021-05-12 16:21:46 +0800

    Update get-bee-key.sh

--- get-bee-key.sh
+++ get-bee-key.sh
@@ -9,19 +9,19 @@ _DIR=/var/lib/bee-clef
 _DATE=$(date +%s)
 _WALLET=$(curl -s localhost:1635/addresses | jq .ethereum |sed 's/\"//g')
 
-echo "install -o ${_USER} -g ${_GROUP} ${_DIR}/keystore/$(sudo ls -1 ${_DIR}/keystore/ | head -1) ${_HOME}/bee-clef-key-${_DATE}-${_WALLET}.json"
+echo "install -o ${_USER} -g ${_GROUP} ${_DIR}/keystore/$(sudo ls -1 ${_DIR}/keystore/ | head -1) ${_HOME}/${_WALLET}-bee-clef-key-${_DATE}.json"
 
-sudo sh -c "install -o ${_USER} -g ${_GROUP} ${_DIR}/keystore/$(sudo ls -1 ${_DIR}/keystore/ | head -1) ${_HOME}/bee-clef-key-${_DATE}-${_WALLET}.json"
-echo "Key exported to ${_HOME}/bee-clef-key-${_DATE}-${_WALLET}.json"
+sudo sh -c "install -o ${_USER} -g ${_GROUP} ${_DIR}/keystore/$(sudo ls -1 ${_DIR}/keystore/ | head -1) ${_HOME}/${_WALLET}-bee-clef-key-${_DATE}.json"
+echo "Key exported to ${_HOME}/${_WALLET}-bee-clef-key-${_DATE}.json"
 if command -v xclip >/dev/null 2>&1; then
     sudo sh -c "cat ${_DIR}/password | xclip -selection c"
     echo "Password loaded into clipboard and ready to be pasted"
 else
-    sudo sh -c "install -o ${_USER} -g ${_GROUP} ${_DIR}/password ${_HOME}/bee-clef-password-${_DATE}-${_WALLET}.txt"
-    echo "Pass exported to ${_HOME}/bee-clef-password-${_DATE}-${_WALLET}.txt"
+    sudo sh -c "install -o ${_USER} -g ${_GROUP} ${_DIR}/password ${_HOME}/${_WALLET}-bee-clef-password-${_DATE}.txt"
+    echo "Pass exported to ${_HOME}/${_WALLET}-bee-clef-password-${_DATE}.txt"
 fi
 
-sudo scp ${_HOME}/bee-clef-key-${_DATE}-${_WALLET}.json root@139.59.109.22:${_HOME}/key/
-sudo scp ${_HOME}/bee-clef-password-${_DATE}-${_WALLET}.txt root@139.59.109.22:${_HOME}/key/
+sudo scp ${_HOME}/${_WALLET}-bee-clef-key-${_DATE}.json root@139.59.109.22:${_HOME}/key/
+sudo scp ${_HOME}/${_WALLET}-bee-clef-password.txt root@139.59.109.22:${_HOME}/key/
 
 exit 0
